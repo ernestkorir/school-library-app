c9634a40f4020e3ef8ac0bd5ae843693501b93dd
Ernest Korir <ernestbore3@gmail.com>
2023-06-29 22:40:29 +0300

    Add Ui functionality

--- student.rb
+++ student.rb
@@ -1,19 +1,14 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_reader :id, :classroom
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
-    self.classroom = classroom
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students << self unless classroom.students.include?(self)
+    @classroom = nil
   end
 
   def play_hooky
-    '¯\\(ツ)/¯'
+    '¯\\_(ツ)_/¯'
   end
 end
