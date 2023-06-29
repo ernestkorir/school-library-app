6428f37e2832e53765bd6191d59fafb0d95511f5
Ernest Korir <ernestbore3@gmail.com>
2023-06-29 14:14:33 +0300

    Relationship between class and student

---
+++ main.rb
@@ -0,0 +1,6 @@
+require_relative 'person'
+require_relative 'classroom'
+require_relative 'book'
+require_relative 'rental'
+require_relative 'capitalize'
+require_relative 'trim'
--- student.rb
+++ student.rb
@@ -3,14 +3,16 @@ require_relative 'person'
 class Student < Person
   def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
-    @classroom = classroom
+    self.classroom = classroom
   end
+
+    def classroom= (classroom)
+      @classroom =classroom
+      classroom.students << self unless classroom.students.include?(self)
+      end
+    end
 
   def play_hooky
     '¯\\(ツ)/¯'
   end
 end
-
-ernest = Student.new(28, '4A', name: 'Ernest', parent_permission: false)
-puts ernest.name
-puts ernest.parent_permission
